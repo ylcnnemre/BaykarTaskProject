38bf3f03353d247fb8551829762b89afe07174d3
emreylc <yalcnnemre@gmail.com>
2022-11-08 11:16:52 +0300

    first commit

---
+++ cssproject/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    <h2 className='text-red-700'>
+      asdadasd
+    </h2>
+   /*  <div  className="container" >
+
+
+      <img src="/images/headergrid.png" alt="" style={{position:"absolute"}} />
+      <div className='header'>
+          <img src="/images/logo.png" alt="" />
+      </div>
+      <h1 className='bg-red-500'>
+        asdasdasdasd
+      </h1>
+    </div> */
+  )
+}
+
+export default App
